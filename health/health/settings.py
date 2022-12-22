e20e422bc84e1237ae4d604c3fc55b0d9acdd06e
mooncw <dusrktl4@gmail.com>
2022-12-22 13:32:24 +0900

    update squat

--- health/health/settings.py
+++ health/health/settings.py
@@ -119,9 +119,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'web', 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'web', 'static')
+# ]
 
 # STATIC_ROOT = [BASE_DIR / "static"]
 
